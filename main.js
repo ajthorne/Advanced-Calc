c8ca7d2a9bbf9d9b0dabf0f6a6dc4b539adfead8
Amanda Thorne <manda.thorne@gmail.com>
2016-06-25 20:00:22 -0500

    functional calculator

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var operatorSign = '';
 var calculator = document.querySelector('section');
 // var numberValue;
-var numberString = '';
+// var numberString = '';
 var buttonClick;
 var number1 = '';
 var number2 = '';
@@ -15,11 +15,8 @@ calculator.addEventListener('click', function(evt) {
     if (operatorSign === '' && number1 === '') {
         number1 = buttonClick;
         //number1 is defined because operatorSign is empty so it recognizes that it is the first number clicked.
-        console.log(number1);
+        // console.log(number1);
     }
-    // numberValue = numberString += buttonClick;
-    // console.log(numberString);
-    //numberString stores items as they are clicked
 
     //define each operator to call the variable later
     if (buttonClick === "+") {
@@ -44,26 +41,31 @@ calculator.addEventListener('click', function(evt) {
 
     if (buttonClick === "=" && operatorSign === "add") {
         console.log(Number(number1) + Number(number2));
+        var sum = document.querySelector('output');
+        sum.innerText += Number(number1) + Number(number2);
+        //performs operation if number1 and number2 are defined and pushes it to innerText of output element
     }
     if (buttonClick === "=" && operatorSign === "subtract") {
         console.log(Number(number1) - Number(number2));
+        var difference = document.querySelector('output');
+        difference.innerText += Number(number1) - Number(number2);
     }
     if (buttonClick === "=" && operatorSign === "multiply") {
         console.log(Number(number1) * Number(number2));
+        var product = document.querySelector('output');
+        product.innerText += Number(number1) * Number(number2);
     }
     if (buttonClick === "=" && operatorSign === "divide") {
         console.log(Number(number1) / Number(number2));
+        var quotient = document.querySelector('output');
+        quotient.innerText += Number(number1) / Number(number2);
     } else {
         number2 = buttonClick;
         console.log(number2);
+        //defines "number2" because if it doesn't meet "number1" criteria then it will fall to "number2" (else)
     }
+
 });
-//defines "number2" because if it doesn't meet "number1" criteria then it will fall to "number2" (else)
-
-
-// numberString.addEventListener('click', function {
-//
-// }
 
 // calculator.addEventListener('click', function(evt) {
 //     console.log(evt);
@@ -76,3 +78,7 @@ calculator.addEventListener('click', function(evt) {
 // if (numberValue.length === 3) {
 //   console.log('hi');
 // }
+
+// numberValue = numberString += buttonClick;
+// console.log(numberString);
+//numberString stores items as they are clicked
